33552337cb87fbac67d5888688a3bc0e13d13c07
digeoesp <diegocrypto@outlook.com>
2021-05-04 21:30:09 -0700

    added object

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'diego';
+  const post = {
+    title : "Dinosaurs are awesome",
+    author : "Stealthy Stegosaurus",
+    body : "Check out this body property",
+    comments : ["First", "Great Post", "Hire this author now"]
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> {name} </p>
+      <h1>{post.title}</h1>
+      <p>by {post.author}</p>
+      <p>{post.body}</p>
+      <h2>Comments:</h2>
+      <p>{post.comments[0]}</p>
     </div>
   );
 }
