9ac377dfa3ac38c2cb7f06bde97e59e9a56ccf8f
digeoesp <diegocrypto@outlook.com>
2021-05-04 21:41:14 -0700

    finished dino lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Comment from './Comment'
 
 function App() {
   const name = 'diego';
@@ -9,14 +10,16 @@ function App() {
     body : "Check out this body property",
     comments : ["First", "Great Post", "Hire this author now"]
   }
+  //map returns an array
+  const comments = post.comments.map(comment=> (<Comment text={comment}/>))
   return (
     <div className="App">
-      <p> {name} </p>
       <h1>{post.title}</h1>
       <p>by {post.author}</p>
       <p>{post.body}</p>
       <h2>Comments:</h2>
       <p>{post.comments[0]}</p>
+      {comments}
     </div>
   );
 }
